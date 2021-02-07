39bede86c6c5b7bbd66c4ca672092533aa64f0bf
Neil Crowley <neilccrowley@gmail.com>
2021-02-07 09:00:31 +0000

    implemented removeAt()

--- LinkedList.java
+++ LinkedList.java
@@ -8,29 +8,28 @@ public class LinkedList {
     }
 
     public void add(int data) {
-
+        /*
+         * Node node = new Node(data);
+         * 
+         * if (first == null) { first = node; last = node; }
+         * 
+         * else { node.setNeighbour(last); last = node; } length += 1;
+         */
         Node node = new Node(data);
-
         if (first == null) {
             first = node;
             last = node;
-        }
-
-        else {
-            node.setNeighbour(last);
+        } else {
+            last.setNeighbour(node);
             last = node;
         }
         length += 1;
+
     }
 
     public void removeFirst(int data) {
         var current = this.first;
         while (current.getNeighbour() != null) {
-            if (current.getData() == data) {
-                current.setNeighbour(null);
-                length -= 1;
-                break;
-            }
             current = current.getNeighbour();
         }
     }
@@ -44,10 +43,29 @@ public class LinkedList {
     }
 
     public void removeAt(int idx) {
-
+        if (idx >= this.length) {
+            return;
+        } else if (idx == 0) {
+            this.first = this.first.getNeighbour();
+        } else {
+            var current = this.first;
+            for (int i = 0; i < idx - 1; i++) {
+                current = current.getNeighbour();
+                current.setNeighbour(current.getNeighbour().getNeighbour());
+            }
+            this.length -= 1;
+        }
     }
 
     public Boolean contains(int data) {
         return false;
+    }
+
+    public void printAll() {
+        var current = this.first;
+        while (current != null) {
+            System.out.println(current.getData());
+            current = current.getNeighbour();
+        }
     }
 }
